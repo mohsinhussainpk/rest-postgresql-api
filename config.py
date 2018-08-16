e20c3a44795ecabd392db20bd94faaefc7ff9aab
Mohsin <mohsinhussain1993@gmail.com>
2018-08-17 02:43:27 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# You need to replace the next values with the appropriate values for your configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = "postgres://djvphbqf:LTlALKyHbxzpQmbVS8KYTFcdos8jjLBG@pellefant.db.elephantsql.com:5432/djvphbqf"
